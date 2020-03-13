a791109621f6258df5c75f139af583a2473bc736
gaetan <gaetan.bloch@gmail.com>
2020-03-13 10:49:00 +0100

    Closes #13 Added Owner Page and Controller

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getOwners() {
+        return "owners/index";
+    }
+}
