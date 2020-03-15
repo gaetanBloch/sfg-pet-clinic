7612786e7804793e19090f71f95ee777e59487c1
gaetan <gaetan.bloch@gmail.com>
2020-03-15 17:27:14 +0100

    fixes #45 Fixed broken links on menu

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
+    }
 }
