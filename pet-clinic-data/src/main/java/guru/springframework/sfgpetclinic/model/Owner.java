48fd7f1276dfe4bb6132e5ffbd2fc8d7a6d0c7aa
gaetan <gaetan.bloch@gmail.com>
2020-03-15 15:43:49 +0100

    fixes #26 Added Visit entity, updated owner with pet relationship

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
