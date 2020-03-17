5151937b2f251acf3511157b37e7113e0db0aa6e
gaetan <gaetan.bloch@gmail.com>
2020-03-17 19:42:38 +0100

    fixes #43 Added spring data jpa services for pets

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService extends AbstractSDJpaService<Pet, PetRepository> implements PetService {
+
+    PetSDJpaService(PetRepository petRepository) {
+        super(petRepository);
+    }
+}
